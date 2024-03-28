cb9eedb152d7d0770ce8127727cd94f5dc1d32d5
Destorvo <dvjmca17@gmail.com>
2024-03-28 08:05:25 -0500

    Segundo avance interfaz mec

--- mec.py
+++ mec.py
@@ -37,7 +37,7 @@ root.geometry('690x900')
 # Inicializar la lista de letras presionadas
 letras_presionadas = []
 
-# Combinaciones permitidas
+# Combinaciones permitidas al escoger tipo de cafe
 combinaciones_permitidas = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2"]
 
 # Funcion para cargar imagenes
@@ -50,7 +50,7 @@ def load_image(ruta, size_x, size_y):
 img_caffeinated= load_image("Images/caffeinated.jpg", 70, 70)
 img_decaffeinated= load_image("Images/decaffeinated.jpg", 70, 70)
 
-# Marco de con cafeina o descafeinado
+# Marco boton con cafeina o descafeinado
 type_of_coffee = Frame(root)
 type_of_coffee.place(x=440,y=190)
 btn_img_caffeinated = Button(type_of_coffee, image=img_caffeinated, command=lambda: btn_presionado("Con cafeina"))
@@ -60,26 +60,26 @@ btn_img_decaffeinated.grid(row=0,column=1,padx=10)
 
 # Marco de botones para seleccionar tipo de cafe
 select_coffee = Frame(root)
-select_coffee.place(x=430,y=290)
+select_coffee.place(x=440,y=290)
 
 # Botones primera fila
-btn_A = Button(select_coffee, text="A", command=lambda: btn_presionado("A"),padx=15,pady=10)
+btn_A = Button(select_coffee, text="A", command=lambda: btn_presionado("A"),padx=25,pady=20)
 btn_A.grid(row=0, column=0)
 
-btn_B = Button(select_coffee, text="B", command=lambda: btn_presionado("B"),padx=15,pady=10)
+btn_B = Button(select_coffee, text="B", command=lambda: btn_presionado("B"),padx=25,pady=20)
 btn_B.grid(row=0, column=1)
 
-btn_C = Button(select_coffee, text="C", command=lambda: btn_presionado("C"),padx=15,pady=10)
+btn_C = Button(select_coffee, text="C", command=lambda: btn_presionado("C"),padx=25,pady=20)
 btn_C.grid(row=0, column=2)
 
 # Botones 1, 2, 3 en la segunda fila
-btn_1 = Button(select_coffee, text="1", command=lambda: btn_presionado("1"),padx=15,pady=10)
+btn_1 = Button(select_coffee, text="1", command=lambda: btn_presionado("1"),padx=25,pady=20)
 btn_1.grid(row=1, column=0)
 
-btn_2 = Button(select_coffee, text="2", command=lambda: btn_presionado("2"),padx=15,pady=10)
+btn_2 = Button(select_coffee, text="2", command=lambda: btn_presionado("2"),padx=25,pady=20)
 btn_2.grid(row=1, column=1)
 
-btn_3 = Button(select_coffee, text="3", command=lambda: btn_presionado("3"),padx=15,pady=10)
+btn_3 = Button(select_coffee, text="3", command=lambda: btn_presionado("3"),padx=25,pady=20)
 btn_3.grid(row=1, column=2)
 
 # Botones con azucar, check y con vainilla en la tercera fila
@@ -107,41 +107,53 @@ canvas.create_rectangle(0, 25, 250, 75, fill="black")
 lbl_texto = Label(root, text="", bg="black", fg="green", font=("Arial", 24))
 lbl_texto.place(x=510, y=130)
 
-# Cargar imagenes de cafe
-img_coffee1 = Image.open("Images/sherk.jpg")
-img_coffee1 = img_coffee1.resize((70, 70))
+# Cargar imagenes de tipos de vaso
+img_coffee_size_small = load_image("Images/coffee_size.png", 50, 50)
 
+# Botones para size del vaso
+type_of_glass = Frame(root)
+type_of_glass.place(x=440,y=550)
+btn_image_coffee_size_small = Button(type_of_glass, image=img_coffee_size_small,command=lambda: btn_presionado("small cup"))
+btn_image_coffee_size_small.grid(row=0,column=0)
+empty = Label(type_of_glass)
+empty.grid(row=0,column=1, padx=20)
+btn_image_coffee_size_medium = Button(type_of_glass, image=img_coffee_size_small,command=lambda: btn_presionado("medium cup"))
+btn_image_coffee_size_medium.grid(row=0,column=2)
 
 
 # FIXME cambiar sherk por imagenes reales
-image_tk = ImageTk.PhotoImage(img_coffee1)
-# Crea Label para mostrar la imagen
-lbl_image = Label(root, image=image_tk)
-lbl_image.grid(row=3, column=0,padx=20,pady=20)
+# 8 bebidas de cafe a elegir
+drink_coffee = Frame(root)
+drink_coffee.place(x=50,y=170)
 
-lbl_img_coffee2 = Label(root, image=image_tk)
-lbl_img_coffee2.grid(row=3, column=1, padx=20,pady=20)
+# Cargar imagenes de cafe
+img_coffee1 = load_image("Images/sherk.jpg", 70, 70)
 
-lbl_img_coffee3 = Label(root, image=image_tk)
-lbl_img_coffee3.grid(row=3, column=2, padx=20,pady=20)
+lbl_img_coffee1 = Label(drink_coffee, image=img_coffee1)
+lbl_img_coffee1.grid(row=0, column=0, padx=20,pady=20)
 
-lbl_img_coffee4 = Label(root, image=image_tk)
-lbl_img_coffee4.grid(row=4, column=0, padx=20,pady=20)
+lbl_img_coffee2 = Label(drink_coffee, image=img_coffee1)
+lbl_img_coffee2.grid(row=0, column=1, padx=20,pady=20)
 
-lbl_img_coffee5 = Label(root, image=image_tk)
-lbl_img_coffee5.grid(row=4, column=1, padx=20,pady=20)
+lbl_img_coffee3 = Label(drink_coffee, image=img_coffee1)
+lbl_img_coffee3.grid(row=0, column=2, padx=20,pady=20)
 
-lbl_img_coffee6 = Label(root, image=image_tk)
-lbl_img_coffee6.grid(row=4, column=2, padx=20,pady=20)
+lbl_img_coffee4 = Label(drink_coffee, image=img_coffee1)
+lbl_img_coffee4.grid(row=1, column=0, padx=20,pady=20)
 
-lbl_img_coffee7 = Label(root, image=image_tk)
-lbl_img_coffee7.grid(row=5, column=0, padx=20,pady=20)
+lbl_img_coffee5 = Label(drink_coffee, image=img_coffee1)
+lbl_img_coffee5.grid(row=1, column=1, padx=20,pady=20)
 
-lbl_img_coffee8 = Label(root, image=image_tk)
-lbl_img_coffee8.grid(row=5, column=1, padx=20,pady=20)
+lbl_img_coffee6 = Label(drink_coffee, image=img_coffee1)
+lbl_img_coffee6.grid(row=1, column=2, padx=20,pady=20)
 
-lbl_img_coffee9 = Label(root, image=image_tk)
-lbl_img_coffee9.grid(row=5, column=2, padx=20,pady=20)
+lbl_img_coffee7 = Label(drink_coffee, image=img_coffee1)
+lbl_img_coffee7.grid(row=2, column=0, padx=20,pady=20)
+
+lbl_img_coffee8 = Label(drink_coffee, image=img_coffee1)
+lbl_img_coffee8.grid(row=2, column=2, padx=20,pady=20)
+
+
 
 # Inicializar ventana
 root.mainloop()
